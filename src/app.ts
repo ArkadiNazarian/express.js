583ad6b0df8eabecf24f017dafb2ce97881f43fc
ArkadiNazarian <nazarianarkadi@gmail.com>
2026-09-08 12:18:25 +0330

    chaining the handlers

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,245 @@
-import express from 'express';
+import express, { type NextFunction, type Request, type Response } from 'express';
 import fs from 'fs/promises';
 import url from 'url';
 
 const server = express();
 
+// this is middleware to parse the body
+server.use(express.json());
+
+// we can have custom middleware like this :
+// each middleware will be called one by one in order with each request
+// for example if we have two middlewares like this
+// server.use(middleware1);
+// server.use(middleware2);
+// then middleware1 will be called first and then middleware2
+// if we want to call middleware2 first then we can use this
+// server.use(middleware2, middleware1);
+// each middleware has 3 parameters
+// req, res, next
+// route handlers also are middlewares but for specific route
+// if the middleware write after a route handler then it will not be called for that route
+
+const logger = (req: Request, res: Response, next: NextFunction) => {
+  console.log('Request started');
+  next();
+}
+
+server.use(logger);
+server.use((req: any, res, next) => {
+  req.createdAt = new Date();
+  next();
+});
+
+// server.get("/api/v1/middlewareTestToPass", (req: any, res) => {
+//   res.status(200).json({
+//     data: {
+//       message: 'Middleware is working',
+//       createdAt: req.createdAt
+//     }
+//   });
+// });
+
 const readFile = fs.readFile('./data/movie.json', 'utf8');
 const movies = JSON.parse(await readFile);
 
 
-// this is get request to rooth path
-server.get('/', (req, res) => {
+// // this is get request to rooth path
+// server.get('/', (req, res) => {
 
 
-  // res.send is used for sending plain text
-  // res.send('Hello World');
+//   // res.send is used for sending plain text
+//   // res.send('Hello World');
 
-  // this is to send also the status code
-  // res.status(200).send('Hello World');
+//   // this is to send also the status code
+//   // res.status(200).send('Hello World');
 
-  // this is to send json
-  res.status(200).json({ message: 'Hello World' });
-});
+//   // this is to send json
+//   res.status(200).json({ message: 'Hello World' });
+// });
 
-// this is middleware to parse the body
-server.use(express.json());
 
-server.post('/api/v1/movies', (req, res) => {
 
-  // req.body is undefiend we should use middleware (line25) to get the body
-  console.log(req.body);
+// server.post('/api/v1/movies', (req, res) => {
 
+//   // req.body is undefiend we should use middleware (line8) to get the body
+//   console.log(req.body);
+
+//   const newMovieId = movies.length + 1;
+
+//   const addedMovie = {
+//     id: newMovieId,
+//     name: req.body.name,
+//     publish_year: req.body.publish_year,
+//     duration: req.body.duration
+//   }
+
+//   movies.push(addedMovie);
+
+//   fs.writeFile('./data/movie.json', JSON.stringify(movies)).then(() => {
+
+//     res.status(201).json({ message: 'Movie added successfully' });
+
+//   }).catch((err) => {
+//     res.status(500).json({ message: 'Error adding movie' });
+//   });
+
+// })
+
+
+
+// server.get('/api/v1/movies', (req, res) => {
+
+//   res.status(200).json({
+//     data: {
+//       movies: movies
+//     }
+//   });
+
+// })
+
+// // get request to specific movie path with id query parameter
+
+// server.get("/api/v1/movies", (req, res) => {
+
+//   const id = url.parse(req.url, true).query.id;
+
+//   res.status(200).json({
+//     data: {
+//       movie: movies.find((movie: any) => movie.id === Number(id))
+//     }
+//   });
+
+// })
+
+// // get request to specific movie path with route parameter
+// // if we want the route parameter to be option we can use this "/api/v1/movie/:id?"
+
+// server.get("/api/v1/movies/:id", (req, res) => {
+
+//   const id = req.params.id;
+
+//   const selectedMovie = movies.find((movie: any) => movie.id === Number(id));
+
+//   if (!selectedMovie) {
+//     res.status(404).json({ message: 'Movie not found' });
+//     return;
+//   }
+
+//   res.status(200).json({
+//     data: {
+//       movie: selectedMovie
+//     }
+//   });
+
+// })
+
+// // to patch a data
+
+// server.patch("/api/v1/movies/:id", (req, res) => {
+//   const id = req.params.id;
+//   const updatedMovie = req.body;
+
+//   const selectedMovie = movies.find((movie: any) => movie.id === Number(id));
+
+//   if (!selectedMovie) {
+//     res.status(404).json({ message: 'Movie not found' });
+//     return;
+//   }
+
+//   const updatedMovieData = {
+//     ...selectedMovie,
+//     ...updatedMovie
+//   }
+
+//   const index = movies.findIndex((movie: any) => movie.id === Number(id));
+//   movies[index] = updatedMovieData;
+
+
+//   fs.writeFile('./data/movie.json', JSON.stringify(movies)).then(() => {
+//     res.status(200).json({
+//       data: {
+//         movie: updatedMovieData
+//       }
+//     });
+//   }).catch((err) => {
+//     res.status(500).json({ message: 'Error updating movie' });
+//   });
+// });
+
+// // to put a data
+// server.put("/api/v1/movies/:id", (req, res) => {
+//   const id = req.params.id;
+//   const updatedMovie = req.body;
+
+//   const selectedMovie = movies.find((movie: any) => movie.id === Number(id));
+
+//   if (!selectedMovie) {
+//     res.status(404).json({ message: 'Movie not found' });
+//     return;
+//   }
+
+//   const updatedMovieData = {
+//     id: selectedMovie.id,
+//     ...updatedMovie
+//   }
+
+//   const index = movies.findIndex((movie: any) => movie.id === Number(id));
+//   movies[index] = updatedMovieData;
+
+//   fs.writeFile('./data/movie.json', JSON.stringify(movies)).then(() => {
+//     res.status(200).json({
+//       data: {
+//         movie: updatedMovieData
+//       }
+//     });
+//   }).catch((err) => {
+//     res.status(500).json({ message: 'Error updating movie' });
+//   });
+// });
+
+// // to delete a data
+
+// server.delete("/api/v1/movie/:id", (req, res) => {
+
+//   const id = req.params.id;
+
+//   const selectedMovie = movies.find((movie: any) => movie.id === Number(id));
+
+//   if (!selectedMovie) {
+//     res.status(404).json({ message: 'Movie not found' });
+//     return;
+//   }
+
+//   const filteredMovies = movies.filter((movie: any) => movie.id !== Number(id));
+
+//   fs.writeFile('./data/movie.json', JSON.stringify(filteredMovies)).then(() => {
+//     res.status(200).json({
+//       data: null
+//     });
+//   }).catch((err) => {
+//     res.status(500).json({ message: 'Error deleting movie' });
+//   });
+
+// })
+
+// *********************************************************************************************
+
+// also we can chaining the route handlers  , if we seperate each callback to a function
+// server.route('/api/v1/movies').get(getMovies).post(addMovie);
+// server.route('/api/v1/movies/:id').get(getMovie).patch(updateMovie).put(updateMovie).delete(deleteMovie);
+// this is the same as above
+
+
+const getMovies = (req: Request, res: Response) => {
+  res.status(200).json({
+    data: {
+      movies: movies
+    }
+  });
+}
+
+const addMovie = (req: Request, res: Response) => {
   const newMovieId = movies.length + 1;
 
   const addedMovie = {
@@ -42,66 +252,23 @@ server.post('/api/v1/movies', (req, res) => {
   movies.push(addedMovie);
 
   fs.writeFile('./data/movie.json', JSON.stringify(movies)).then(() => {
-
     res.status(201).json({ message: 'Movie added successfully' });
-
   }).catch((err) => {
     res.status(500).json({ message: 'Error adding movie' });
   });
+}
 
-})
-
-
-
-server.get('/api/v1/movies', (req, res) => {
-
-  res.status(200).json({
-    data: {
-      movies: movies
-    }
-  });
-
-})
-
-// get request to specific movie path with id query parameter
-
-server.get("/api/v1/movie", (req, res) => {
-
-  const id = url.parse(req.url, true).query.id;
+const getMovie = (req: Request, res: Response) => {
+  const id = req.params.id;
 
   res.status(200).json({
     data: {
       movie: movies.find((movie: any) => movie.id === Number(id))
     }
   });
+}
 
-})
-
-// get request to specific movie path with route parameter
-// if we want the route parameter to be option we can use this "/api/v1/movie/:id?"
-
-server.get("/api/v1/movie/:id", (req, res) => {
-
-  const id = req.params.id;
-
-  const selectedMovie = movies.find((movie: any) => movie.id === Number(id));
-
-  if (!selectedMovie) {
-    res.status(404).json({ message: 'Movie not found' });
-    return;
-  }
-
-  res.status(200).json({
-    data: {
-      movie: selectedMovie
-    }
-  });
-
-})
-
-// to patch a data
-
-server.patch("/api/v1/movie/:id", (req, res) => {
+const updateMovie = (req: Request, res: Response) => {
   const id = req.params.id;
   const updatedMovie = req.body;
 
@@ -120,7 +287,6 @@ server.patch("/api/v1/movie/:id", (req, res) => {
   const index = movies.findIndex((movie: any) => movie.id === Number(id));
   movies[index] = updatedMovieData;
 
-
   fs.writeFile('./data/movie.json', JSON.stringify(movies)).then(() => {
     res.status(200).json({
       data: {
@@ -130,43 +296,9 @@ server.patch("/api/v1/movie/:id", (req, res) => {
   }).catch((err) => {
     res.status(500).json({ message: 'Error updating movie' });
   });
-});
+}
 
-// to put a data
-server.put("/api/v1/movie/:id", (req, res) => {
-  const id = req.params.id;
-  const updatedMovie = req.body;
-
-  const selectedMovie = movies.find((movie: any) => movie.id === Number(id));
-
-  if (!selectedMovie) {
-    res.status(404).json({ message: 'Movie not found' });
-    return;
-  }
-
-  const updatedMovieData = {
-    id: selectedMovie.id,
-    ...updatedMovie
-  }
-
-  const index = movies.findIndex((movie: any) => movie.id === Number(id));
-  movies[index] = updatedMovieData;
-
-  fs.writeFile('./data/movie.json', JSON.stringify(movies)).then(() => {
-    res.status(200).json({
-      data: {
-        movie: updatedMovieData
-      }
-    });
-  }).catch((err) => {
-    res.status(500).json({ message: 'Error updating movie' });
-  });
-});
-
-// to delete a data
-
-server.delete("/api/v1/movie/:id", (req, res) => {
-
+const deleteMovie = (req: Request, res: Response) => {
   const id = req.params.id;
 
   const selectedMovie = movies.find((movie: any) => movie.id === Number(id));
@@ -185,8 +317,11 @@ server.delete("/api/v1/movie/:id", (req, res) => {
   }).catch((err) => {
     res.status(500).json({ message: 'Error deleting movie' });
   });
+}
 
-})
+server.route('/api/v1/movies').get(getMovies).post(addMovie);
+server.route('/api/v1/movies/:id').get(getMovie).patch(updateMovie).put(updateMovie).delete(deleteMovie);
+
 
 server.listen(3000, () => {
   console.log('Server is running on port 3000');
