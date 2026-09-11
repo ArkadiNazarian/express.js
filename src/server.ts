1dd7159243d0aae988a83f8c014a389e834a2fff
ArkadiNazarian <nazarianarkadi@gmail.com>
2026-09-11 19:06:58 +0330

    add the mongodb

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,19 @@
 import { server } from './app.js';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+
 
 // we can use dotenv to load the environment variables then we can use in the project
 dotenv.config();
+
+// connect to the database
+mongoose.connect(process.env.MONGODB_URI!).then((connection) => {
+  console.log('Connected to the database');
+}).catch((error) => {
+  console.log('Error connecting to the database');
+});
+
 
 server.listen(process.env.PORT, () => {
   console.log('Server is running on port 3000');
